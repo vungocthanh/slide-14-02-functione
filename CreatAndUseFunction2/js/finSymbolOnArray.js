44833555788567e42bb5c9b5b5906d5218ca34a8
ThanhDev <vuphamngocthanh@gmail.com>
2022-08-23 00:12:48 +0700

    Update tìm kí tự trong mang

--- CreatAndUseFunction2/js/finSymbolOnArray.js
+++ CreatAndUseFunction2/js/finSymbolOnArray.js
@@ -4,14 +4,16 @@ var arrSymbol = new Array(n);
 for ( var i = 0 ; i < n ; i++ ){
     arrSymbol[i] = prompt("Enter a Symbol")
 }
+var count = 0;
 function checkSymbol(arrSymbol){
     var checkItem = prompt('Enter a Symbol to Check')
-    for ( var i = 0 ,count = 0 ; i < arrSymbol.length ; i++ ){
+    for ( var i = 0 ; i < arrSymbol.length ; i++ ){
         if( checkItem == arrSymbol[i]){
             var checked = "True";
-            var count++;
+            count++;
         }
     }
-    return "The Symbol" + checked + " " + " so lan xuat hien" + count;
+    return "The Symbol " + '"' + checkItem + '"' + " có trong mảng ="+ checked + " " + " xuất hiện " + count + " lần ";
 }
+document.getElementById('exp').innerHTML = arrSymbol;
 alert(checkSymbol(arrSymbol));
